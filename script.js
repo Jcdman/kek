4cfff0c06ea0064da9790b3742c03aade42a3bd1
Jcdman <122246670+Jcdman@users.noreply.github.com>
2023-01-08 23:39:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-let soundeffect = new Audio('/audio/check.mp3')
+let soundeffect = new Audio('https://github.com/Jcdman/kek/blob/master/audio/check.mp3?raw=true')
 while (true) {
     let answer = confirm("Do you want to allow this website from an unknown publisher to make changes to your device?");
     if (answer == true) {
