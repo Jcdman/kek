e2fb2c98e5b6859c2c7f79b871b01d2a33e1b826
Jcdman <122246670+Jcdman@users.noreply.github.com>
2023-01-09 00:39:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-
-let soundeffect = new Audio('https://github.com/Jcdman/kek/blob/master/audio/check.mp3?raw=true')
 while (true) {
     let answer = confirm("Do you want to allow this website from an unknown publisher to make changes to your device?");
     if (answer == true) {
         break;
     }
 }
-function PlaySound() {
-    let soundeffect = new Audio('/audio/check.mp3')
-    soundeffect.play();
-}
 
+setTimeout(5000);
 window.onload=function(){
-    var button = document.getElementById("funnybutton")
-    button.addEventListener("mouseover", button.click());
-    button.addEventListener("mouseover", PlaySound());
+    document.querySelector(".funny").click();
+    var button = document.getElementById("funnybutton");
+    button.addEventListener("mouseover", function() {
+        button.click();
+        let soundeffect = new Audio('https://github.com/Jcdman/kek/blob/master/audio/check.mp3?raw=true')
+        soundeffect.play();
+    });
+    const kekbutton = document.querySelector(".kekbutton")
+    kekbutton.addEventListener("click", function() {
+        let funnysong = new Audio("https://github.com/Jcdman/kek/blob/master/audio/Troll%20Song.mp3?raw=true")
+        funnysong.play();
+        let soundeffect = new Audio('https://github.com/Jcdman/kek/blob/master/audio/check.mp3?raw=true')
+        soundeffect.play();
+    })
 };
-
